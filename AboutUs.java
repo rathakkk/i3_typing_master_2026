070699e163fe317e8019527521d661b396e13e5b
Chin Rothsophea <rothsopheachin@gmail.com>
2026-03-23 16:41:41 +0700

    Add AboutUs page

---
+++ AboutUs.java
@@ -0,0 +1,12 @@
+public class AboutUs {
+
+    public static void show() {
+        System.out.println("\n------ ABOUT US ------");
+        System.out.println("I3 Typing Master v2026");
+        System.out.println("Developed for improving typing speed.");
+    }
+
+    public static void main(String[] args) {
+        show();
+    }
+}
